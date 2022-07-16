52480445e84b95e12e93ad092a1c0748459dd56f
William May <wjrm500@gmail.com>
2022-07-16 10:03:28 +0800

    Problem sheet 2.1 - Recursion on numbers - a

--- problems-2.1.hs
+++ problems-2.1.hs
@@ -24,8 +24,15 @@ leetSpeak (c:cs)
 
 factors2 :: Int -> [Int]
 factors2 0 = []
-factors2 n | (n `mod` 2 == 0) =  2 : factors2 (n `div` 2)
-           | otherwise        = [n]
+factors2 n
+    | even n = 2 : factors2 (n `div` 2)
+    | otherwise = [n]
+        
+factorsm :: Int -> Int -> [Int]
+factorsm m 0 = []
+factorsm m n
+    | (n `mod` m == 0) = m : factorsm m (n `div` m)
+    | otherwise = [n]
 
 factorsFrom :: Int -> Int -> [Int]
 factorsFrom _ 0 = []
